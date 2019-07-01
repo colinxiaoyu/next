772d7f98441adce1723c95fa56bec946dd0a5fc0
Colin <yuheng_0710@outlook.com>
2019-07-01 10:02:03 +0800

    1.draw UI 2.make navigation

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {Text, View, AsyncStorage, SafeAreaView} from "react-native";
+import TextAdd from "./TextAdd";
+import ColorButton from "./ColorButton";
 
 
 
@@ -9,14 +11,40 @@ export default class Main extends React.Component {
     title: 'Main'
   };
 
+  state = {
+    username: 'Colin',
+    password: '123',
+  }
 
+  _next = () => {
+    this.props.navigation.navigate('Select')
+  }
 
 
   render () {
 
     return (
       <SafeAreaView style={{flex: 1, alignItems: "center",}}>
-        <Text>Main</Text>
+        <TextAdd
+          title={'username'} placeholder={'please tap in username'}
+          onChangeText={(text) => {
+            this.setState({username: text});
+          }}
+        />
+        <TextAdd
+          title={'password'} placeholder={'please tap in password'}
+          onChangeText={(text) => {
+            this.setState({password: text});
+          }}
+        />
+
+        <ColorButton
+          text={' Create Account '}
+          onPress={() => {}}/>
+
+        <ColorButton
+          text={' NextPage '}
+          onPress={() => this._next()}/>
       </SafeAreaView>
     );
   }
--- src/Select.js
+++ src/Select.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {SafeAreaView, Text, AsyncStorage} from "react-native";
+import TextAdd from "./TextAdd";
+import ColorButton from "./ColorButton";
 
 export default class Select extends React.Component {
 
@@ -7,17 +9,35 @@ export default class Select extends React.Component {
     title: 'Select'
   };
 
-
-
-
-
+  state = {
+    text: '',
+    username: '',
+    password: '',
+  }
 
 
   render () {
     const {text} = this.state;
     return (
       <SafeAreaView style={{flex: 1, alignItems: "center",}}>
-        <Text>Select</Text>
+        <TextAdd
+          title={'username'} placeholder={'please tap in username'}
+          onChangeText={(text) => {
+            this.setState({username: text});
+          }}
+        />
+        <TextAdd
+          title={'password'} placeholder={'please tap in password'}
+          onChangeText={(text) => {
+            this.setState({password: text});
+          }}
+        />
+
+        <ColorButton
+          text={' Get privateKey '}
+          onPress={() => {}}/>
+
+        <Text>{text}</Text>
 
       </SafeAreaView>
     )
